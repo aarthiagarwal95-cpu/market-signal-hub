aec2f8b2d20c32a99b9c471142934ae5530ff2ff
aarthiagarwal95-cpu <aarthiagarwal.95@gmail.com>
2025-08-06 16:53:39 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
